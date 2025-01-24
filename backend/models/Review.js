4e13d09c4629ed36e80f33859f041b61eb266607
Ayushmaurya <139100630+Sarcastic21@users.noreply.github.com>
2025-01-24 10:00:19 +0530

    Update Review.js

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,13 +1,13 @@
-// models/Review.js
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const reviewSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
   name: { type: String, required: true },
   comment: { type: String, required: true },
   rating: { type: Number, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Review', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
+export default Review;
